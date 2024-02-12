fde6dab9efe5eaec8ae1048af22ebcc4a55635e7
rimanshusingh4 <rimanshusingh4@gmail.com>
2024-02-12 10:58:06 +0530

    jkhj

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -4,9 +4,7 @@ import Input from './Input'
 import Button from './Button'
 import { useNavigate } from 'react-router-dom';
 import { account } from '../config/Auth';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
+// function Signup() {
 const Signup = ()=>{
     const [userData, setUserData] = useState({email:"",password:"", name:""});
     const navigate = useNavigate();
@@ -18,19 +16,21 @@ const Signup = ()=>{
             userData.name
         )
         signPromis.then(function(res){
+            // console.log("Responce of signpromis",res);
             if(res.status===true){
-                toast.success("Account Created Successfully, Verification Email send to Your Email.");
+                alert("Account Created Successfully, Verification Email send to Your Email.");
                 verify();
+                // console.log("Userdata before API call",userData)
             }
             else{
-                toast.error("There are some Problem please try Again");
+                alert("There are some Problem please try Again");
             }
         },function(err){
-            console.log(err)
+            console.log(signPromis)
         })
         const verify= async () =>{
             let session = await account.createEmailSession(userData.email,userData.password);
-            let verifyPromise = await account.createVerification("https://ever-ecru.vercel.app/Verify");
+            let verifyPromise = await account.createVerification("http://localhost:5173/Verify");
         }
     }
   return (
@@ -38,6 +38,7 @@ const Signup = ()=>{
         <Input onChange={(e)=> setUserData({...userData, name:e.target.value})} label="name" type='text' name='name' placeholder='Enter Full Name' className='w-3/5 border  border-gray-600 p-5 mt-3 rounded-lg' required/>
         <Input onChange={(e)=> setUserData({...userData, email:e.target.value})} label="email" type='email' name='email' placeholder='Enter Email Id' className='w-3/5 border border-gray-600 p-5 mt-3 rounded-lg' required/>
         <Input onChange={(e)=> setUserData({...userData, password:e.target.value})} label="password" type='password' name='password' placeholder='Enter Password' className='w-3/5 border border-gray-600 p-5 mt-3 rounded-lg' required/>
+
         <Button onClick={signFunc} className='w-1/3 mb-6 bg-gray-700 hover:bg-orange-700 mt-3'>Signup</Button>
     </div>
   )
--- src/config/Auth.js
+++ src/config/Auth.js
@@ -2,8 +2,8 @@ import {Client, Account, Databases} from 'appwrite'
 
 const client = new Client();
 
-client.setEndpoint("https://cloud.appwrite.io/v1").setProject("65c2315506f6d412f284");
+client.setEndpoint("https://cloud.appwrite.io/v1").setProject("65c9ab78bb0f3dbd188e");
 
 export const account = new Account(client);
 
-export const database  = new Databases(client, "65c23bbeb2f601b75f65")
+export const database  = new Databases(client, "65c9abf47a8df89faa92")
